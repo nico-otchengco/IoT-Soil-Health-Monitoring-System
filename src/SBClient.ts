0ce4795aa64cbb7e444a353bc538ee540ea196a6
nico-otchengco <otchengco.nico@gmail.com>
2026-04-12 13:09:09 +0800

    fix persist rerouting on reload

--- src/SBClient.ts
+++ src/SBClient.ts
@@ -3,4 +3,11 @@ import { createClient } from '@supabase/supabase-js';
 const url = import.meta.env.VITE_SB_URL as string;
 const key = import.meta.env.VITE_SB_ANON_KEY as string;
 
-export const sb = createClient(url, key);
+export const sb = createClient(url, key, {
+  auth: {
+    persistSession: true,
+    storageKey: 'smartsoil',
+    storage: window.localStorage,
+    autoRefreshToken: true,
+  }
+});
